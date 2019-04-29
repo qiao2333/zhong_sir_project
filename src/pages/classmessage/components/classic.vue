9fb6abdbef39c9e9265954b0ba6f8fe3c0ae9898
520333 <1558684603@qq.com>
2019-04-29 23:11:28 +0800

    测试

--- src/pages/classmessage/components/classic.vue
+++ src/pages/classmessage/components/classic.vue
@@ -1,13 +1,21 @@
 <template>
-	<div id="app">
-		班级信息
-		
-	</div>
+  <div class="content">
+    <a-input-search
+      placeholder="输入班级"
+      style="width: 200px"
+      @search="onSearch"
+    />
+    
+  </div>
 </template>
 
 <script>
-	
+import ExportJsonExcel from 'js-export-excel';
+export default {
+  methods: {
+    onSearch (value) {
+      console.log(value)
+    },
+  },
+}
 </script>
-
-<style>
-</style>
