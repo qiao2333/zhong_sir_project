fd22e1f67f41464670df4a89db24567129186f92
520333 <1558684603@qq.com>
2019-04-30 12:04:18 +0800

    gengxin

--- src/pages/classmessage/components/classic.vue
+++ src/pages/classmessage/components/classic.vue
@@ -6,9 +6,24 @@
       @search="onSearch"
     />
     
-  </div>
-</template>
+  
+	<a-card title="16软一">
+	  <a-card-grid style="width:25%;textAlign:'center'">姓名</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">学号</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">性别</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">宿舍</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">乔哥</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">201624133103</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">男</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">5区102</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">{{user.user_name}}</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">{{student.stu_no}}</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">{{user.user_sex}}</a-card-grid>
+	  <a-card-grid style="width:25%;textAlign:'center'">{{student.live_room}}</a-card-grid>
+	</a-card>
+</div>
 
+</template>
 <script>
 import ExportJsonExcel from 'js-export-excel';
 export default {
@@ -17,5 +32,31 @@ export default {
       console.log(value)
     },
   },
+  data() {
+			return {
+				hasload: false,
+				loading: true,
+				student: {
+					stu_no: "未加载",
+					live_room: "未加载"
+				},
+				user: {
+					user_name: "未加载",
+					user_sex: "未加载",
+				},
+			}
+		},
+		mounted() {
+			this.$axios.post('/classmessage/classic').then((res) => {
+				const data = res.data
+				this.student = data.data.student
+				this.user = data.data.user
+				this.hasload = true
+				this.loading = false
+			}).catch((err) => {
+				console.log(err)
+			})
+		},
 }
 </script>
+
