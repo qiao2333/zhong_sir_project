62fce919180538779aba4ccc8a8c9fbb964b8806
qiao <1170793857@qq.com>
2019-06-11 20:56:59 +0800

    更新

--- src/pages/BaseInfo/classmessage/components/HeightSearch.vue
+++ src/pages/BaseInfo/classmessage/components/HeightSearch.vue
@@ -25,17 +25,17 @@
 						</a-collapse-panel>
 						<a-collapse-panel :forceRender="true" header="班级" key="3">
 							<a-form-item>
-								<a-checkbox-group v-decorator="['className']"  :options="forms.classNameOptions" />
+								<a-checkbox-group v-decorator="['className']" :options="forms.classNameOptions" />
 							</a-form-item>
 						</a-collapse-panel>
 						<a-collapse-panel :forceRender="true" header="岗位" key="4">
 							<a-form-item>
-								<a-checkbox-group v-decorator="['position']"  :options="forms.positionOptions" />
+								<a-checkbox-group v-decorator="['position']" :options="forms.positionOptions" />
 							</a-form-item>
 						</a-collapse-panel>
 						<a-collapse-panel :forceRender="true" header="政治面貌" key="5">
 							<a-form-item>
-								<a-checkbox-group v-decorator="['political']"  :options="forms.pliticalOptions" />
+								<a-checkbox-group v-decorator="['political']" :options="forms.pliticalOptions" />
 							</a-form-item>
 						</a-collapse-panel>
 					</a-collapse>
@@ -53,21 +53,16 @@
 		data() {
 			return {
 				forms: {
-					pliticalOptions: [{label:'团员',value:0},{label:'群众',value:1},{label:'党员',value:2}],
-					positionOptions: [{label:'班长',value:0},{label:'学习委员',value:1},{label:'体育委员',value:2},],
-					classNameOptions: [{label:'16软一',value:0},{label:'15科一',value:1},{label:'16软二',value:2}],
-					gradeOptions: [{label:'2015',value:0},{label:'2016',value:1},{label:'2017',value:2}],
-					specialtyOptions: [{label:'软件工程',value:0},{label:'大数据',value:1},{label:'计算机科学',value:2}],
-					user_sexOptions: [{
-						label: '男',
-						value: 0
-					}, {
-						label: '女',
-						value: 1
-					}, {
-						label: '不详',
-						value: 2
-					}],
+					pliticalOptions: [],
+					positionOptions: [],
+					classNameOptions: [],
+					gradeOptions: [],
+					specialtyOptions: [],
+					user_sexOptions: [
+						'男',
+						'女',
+						'不详',
+					],
 
 				},
 				drawerOpen: false,
@@ -76,7 +71,17 @@
 		},
 		methods: {
 			opendrawer() {
-				this.drawerOpen = true
+				this.axios.get("/json/employee/filter/all?userId=" + 2106).then((res) => {
+					this.forms.pliticalOptions = res.data.filterMessage.politicalName
+					this.forms.positionOptions = res.data.filterMessage.positionName
+					this.forms.classNameOptions = res.data.filterMessage.className
+					this.forms.gradeOptions = res.data.filterMessage.gradeName
+					this.forms.specialtyOptions = res.data.filterMessage.specialtyName
+					this.drawerOpen = true
+				}).catch((err) => {
+					console.log(err)
+				})
+
 			},
 			onClose() {
 				this.drawerOpen = false
@@ -86,8 +91,9 @@
 				e.preventDefault();
 				this.myform.validateFields((err, values) => {
 					if (!err) {
-						var obj = {...values}
-						obj = this.$lodash.omitBy(obj,this.$lodash.isEmpty)
+						var obj = { ...values
+						}
+						obj = this.$lodash.omitBy(obj, this.$lodash.isEmpty)
 						console.log(obj)
 					}
 				});
